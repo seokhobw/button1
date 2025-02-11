2e068a8b4159a4c53968677eaf2bc5ad4f036703
Seokho Lee <joseph000617@gmail.com>
2025-02-12 00:38:25 +0900

    seco

--- button1.py
+++ button1.py
@@ -5,11 +5,8 @@ window =Tk()
 b1 = Button(window, text="Seok HO Lee")
 b2 = Button(window, text="60222118")
 b1.grid(row=0, column=0)
-b2.grid(row=1, column=1)
-
-window.mainloop()
-
-print("ehel")
-''
+b2.grid(row=1, column=0)
 
 print("heje")
+
+print("good")
